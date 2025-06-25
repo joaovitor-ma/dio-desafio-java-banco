2747dc5e5e172ac118fbe86fdd9dadf542fb8641
João Vitor <jvitorcavalcante18@gmail.com>
2025-06-25 13:18:44 -0300

    refactor: Criando Classe Abstrata Conta
    Foi criada uma classe abstrata Conta que será usada como a classe mãe da
    conta poupança e da conta corrente, assim, evitando a repetição de
    código.

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -1,45 +1,7 @@
-import Exceptions.SaldoInsuficienteException;
-
-public class ContaCorrente {
-    private final int agencia;
-    private final int numero;
-    private double saldo;
+public class ContaCorrente extends Conta {
 
     public ContaCorrente(int agencia, int numero) {
-        this.agencia = agencia;
-        this.numero = numero;
-        this.saldo = 0;
+        super(agencia, numero);
     }
 
-    public void sacar(double valor) {
-        if(this.saldo - valor >= 0) {
-            this.saldo -= valor;
-        }else{
-            throw new SaldoInsuficienteException("Você não possui saldo o suficiente para realizar o saque.");
-        }
-    }
-
-    public void depositar(double valor) {
-        this.saldo += valor;
-    }
-
-    public void transferir(int numero, double valor) {
-        if(this.saldo - valor >= 0){
-            // TODO: criar alguma forma de retirar o saldo da conta atual e enviar para a conta passada nos parametros
-        }else{
-            throw new SaldoInsuficienteException("Você não possui saldo o suficiente para realizar a transferência.");
-        }
-    }
-
-    public int getAgencia() {
-        return agencia;
-    }
-
-    public int getNumero() {
-        return numero;
-    }
-
-    public double getSaldo() {
-        return saldo;
-    }
 }
